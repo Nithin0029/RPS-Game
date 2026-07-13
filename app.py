913df596204a272f95d911f0db75d371e8e3d1ff
Nithin0029 <nithelanjayakumar@gmail.com>
2026-07-13 12:04:51 +0530

    Logic and Css added

--- app.py
+++ app.py
@@ -1,10 +1,57 @@
-from flask import Flask
+from flask import Flask, session, render_template, redirect, url_for, request
+
+import random
 
 app = Flask(__name__)
+app.secret_key = 'replace-with-a-secret-key'
 
-@app.route('/')
+choices = ['rock', 'paper', 'scissors']
+
+def get_winner(user, computer):
+    if user == computer:
+        return "tie"
+    elif (user == 'rock' and computer == 'scissors') or \
+         (user == 'paper' and computer == 'rock') or \
+         (user == 'scissors' and computer == 'paper'):
+        return "user wins!"
+    else:
+        return "computer wins!"
+
+@app.route('/home')
 def home():
-    return "Welcome to Rock Paper Scissor RPS Game!"
+    if 'wins' not in session:
+        session["wins"] = 0
+        session["losses"] = 0
+        session["draws"] = 0
+
+    return render_template('index.html', session=session)
+
+@app.route('/play',methods=['POST'])
+def play():
+    user_choice = request.form['choice']
+    computer_choice = random.choice(choices)
+    winner = get_winner(user_choice, computer_choice)
+
+    if winner == 'computer wins!':
+        session['losses'] += 1
+    elif winner == 'user wins!':
+        session['wins'] += 1
+    else:
+        session['draws'] += 1
+    return render_template('index.html',
+        user_choices=user_choice,
+        computer_choices=computer_choice,
+        winner=winner,
+        session=session
+    )
+
+@app.route('/reset')
+def reset():
+    session['wins'] = 0
+    session['losses'] = 0
+    session['draws'] = 0
+    return redirect(url_for('home'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
